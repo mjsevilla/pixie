46703f83a25d66003e81c2eef0975037d3c23212
cjavier70 <rocker5893@yahoo.com>
2015-02-19 12:41:40 -0800

    finishing touches

--- welcome_get.php
+++ welcome_get.php
@@ -2,8 +2,10 @@
 <body>
 
 Start Point: <?php echo $_GET["start"]; ?><br>
-End Point: <?php echo $_GET["end"]; ?>
-Day: <?php echo $_GET["day"]; ?>
+End Point: <?php echo $_GET["end"]; ?><br>
+Day: <?php echo $_GET["day"]; ?><br>
+Time: <?php echo $_GET["time"]; ?><br>
+Driver/Rider: <?php echo $_GET["driverEnum"]; ?><br>
 
 
 <?php
@@ -29,14 +31,20 @@ Day: <?php echo $_GET["day"]; ?>
 	VALUES ('$start', '$end', '$day', '$driverEnum', '$time')";
 	if ($conn->query($sql) === TRUE) 
 	{
-		echo "New record created successfully";
+		echo "New record created successfully in DBS!!!!";
 	} 
 	else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
+		echo "MySQL Error: " . $sql . "<br>" . $conn->error;
 	}
 
 	$conn->close();
 ?>
+
+To observe populated database...
+hostname = aaiblrud1k2f1u.c8ktrid1mjul.us-west-2.rds.amazonaws.com<br>
+username = php<br>
+password = pixiedust<br>
+dbname = innodb<br>
 
 </body>
 </html>
