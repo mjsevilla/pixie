2f963089401b31c935834ebaf17cdeb39d08c20a
Mike Sevilla <michaelsevilla@gmail.com>
2015-04-27 22:08:20 -0700

    added Parse push shit that doesn't fucking work cuz it's not iOS 8.* compatable fuck you

--- mike's stuff/Pixie/AppDelegate.swift
+++ mike's stuff/Pixie/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Parse stuff
+//        Parse.setApplicationId("U9G1lVXQ2AVcJTLhtCOGf2Zwk0Q1ZQ47jSHqpoEy",
+//            clientKey: "Fs4ajHom8FkqZYYv4OJCWUA1Yn2dGhunYViSD6SI")
+//        // Register for Push Notitications
+//        if application.applicationState != UIApplicationState.Background {
+//            // Track an app open here if we launch with a push, unless
+//            // "content_available" was used to trigger a background push (introduced in iOS 7).
+//            // In that case, we skip tracking here to avoid double counting the app-open.
+//            
+//            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+//            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+//            var pushPayload = false
+//            if let options = launchOptions {
+//                pushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil
+//            }
+//            if (preBackgroundPush || oldPushHandlerOnly || pushPayload) {
+//                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+//            }
+//        }
+//        if application.respondsToSelector("registerUserNotificationSettings:") {
+//            let userNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+//            let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+//            application.registerUserNotificationSettings(settings)
+//            application.registerForRemoteNotifications()
+//        } else {
+//            let types = UIRemoteNotificationType.Badge | UIRemoteNotificationType.Alert | UIRemoteNotificationType.Sound
+//            application.registerForRemoteNotificationTypes(types)
+//        }
+        
+        // Facebook stuff
         FBLoginView.self
         FBProfilePictureView.self
         return true
@@ -48,6 +80,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+//    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+//        let installation = PFInstallation.currentInstallation()
+//        installation.setDeviceTokenFromData(deviceToken)
+//        installation.saveInBackground()
+//    }
+//    
+//    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+//        if error.code == 3010 {
+//            println("Push notifications are not supported in the iOS Simulator.")
+//        } else {
+//            println("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+//        }
+//    }
+//    
+//    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+//        PFPush.handlePush(userInfo)
+//        if application.applicationState == UIApplicationState.Inactive {
+//            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+//        }
+//    }
 
 }
 
