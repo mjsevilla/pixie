7a71bd6577b3f880f86095fea687a7d57d51b809
Mike Sevilla <michaelsevilla@gmail.com>
2015-04-30 13:14:15 -0700

    messaging stuffs

--- mike's stuff/Pixie/MessagesViewController.swift
+++ mike's stuff/Pixie/MessagesViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class MessagesViewContoller: UIViewController, UITableViewDelegate {
+class MessagesViewContoller: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var messagesTable: UITableView!
     var navTransitionOperator = NavigationTransitionOperator()
@@ -20,9 +20,10 @@ class MessagesViewContoller: UIViewController, UITableViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         
         messagesTable.delegate = self
+        messagesTable.dataSource = self
         
-//        let temp = MessageModel(name: "Julio Coolio", lastMsg: "hi", timestamp: "10:00")
-//        messages.append(temp)
+        let temp = MessageModel(name: "Julio Coolio", lastMsg: "hella long message that will be cut off at the end with triple dots")
+        messages = [temp]
         
         var rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
         rightSwipe.direction = .Right
@@ -53,6 +54,36 @@ class MessagesViewContoller: UIViewController, UITableViewDelegate {
         return UIStatusBarStyle.LightContent
     }
     
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            messages.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MessageCell") as! MessageCell
+        let message = messages[indexPath.row]
+        
+        
+        cell.nameLabel.text = message.name
+        if count(message.lastMsg) > 50 {
+            let lastMsg = message.lastMsg as NSString
+            let shortMsg = lastMsg.substringWithRange(NSRange(location: 0, length: 50)) + "..."
+            cell.lastMessageLabel.text = shortMsg
+        }
+        else {
+            cell.lastMessageLabel.text = message.lastMsg
+        }
+        
+        return cell
+    }
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
     }
@@ -62,11 +93,16 @@ class MessageModel {
     
     var name: String!
     var lastMsg: String!
-    var timestamp: String!
+    var timestamp: String?
     
     init(name: String, lastMsg: String, timestamp: String) {
         self.name = name
         self.lastMsg = lastMsg
         self.timestamp = timestamp
+    }
+    
+    init(name: String, lastMsg: String) {
+        self.name = name
+        self.lastMsg = lastMsg
     }
 }
