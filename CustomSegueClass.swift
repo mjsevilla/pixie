052089bd5b3b7dd314585c05cd08fa8004e34e33
Mike Sevilla <michaelsevilla@gmail.com>
2015-05-13 12:06:44 -0700

    sup

--- CustomSegueClass.swift
+++ CustomSegueClass.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomSegueClass: UIStoryboardSegue {
+    override func perform() {
+        var sourceViewController: UIViewController = self.sourceViewController as! UIViewController
+        var destinationViewController: UIViewController = self.destinationViewController as! UIViewController
+        sourceViewController.view.addSubview(destinationViewController.view)
+        destinationViewController.view.removeFromSuperview()
+        
+        // Force presentViewController() into a different runloop.
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(0.001 * Double(NSEC_PER_SEC)))
+        dispatch_after(time, dispatch_get_main_queue()) {
+            sourceViewController.presentViewController(destinationViewController, animated: true, completion: nil)
+        }
+    }
+}
