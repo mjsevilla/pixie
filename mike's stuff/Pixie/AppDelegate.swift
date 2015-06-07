9aedabdaab777908d1b3694e45e4868b0b93922c
Mike Sevilla <michaelsevilla@gmail.com>
2015-06-07 14:54:43 -0700

    appdelegate print

--- mike's stuff/Pixie/AppDelegate.swift
+++ mike's stuff/Pixie/AppDelegate.swift
@@ -112,6 +112,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         let installation = PFInstallation.currentInstallation()
         installation.setDeviceTokenFromData(deviceToken)
+        println(deviceToken)
         installation.saveInBackground()
     }
     
