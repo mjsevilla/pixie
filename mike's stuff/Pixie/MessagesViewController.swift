ea6bd01930c5cde97b4f10190a449e6bf9766697
cjavier70 <rocker5893@yahoo.com>
2015-06-09 23:55:11 -0700

    I think im done with messageviewcontroller recursive runction call getConversation

--- mike's stuff/Pixie/MessagesViewController.swift
+++ mike's stuff/Pixie/MessagesViewController.swift
@@ -14,6 +14,7 @@ class MessagesViewContoller: UITableViewController {
     let user = PFUser.currentUser()!
     let currentInstallation = PFInstallation.currentInstallation()
     var convos: [PFObject]?
+    var timer: NSTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -164,5 +165,15 @@ class MessagesViewContoller: UITableViewController {
         
         return time
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: Selector("callLoadConversations"), userInfo: nil, repeats: true)
+    }
+    func callLoadConversations() {
+        print("callLoadMessages")
+        loadConversations()
+    }
+
 }
 
